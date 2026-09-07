5c0f51e43fed82697aa09b9f569df673389e1eac
William Lempinen <william.lempinen@twoday.com>
2026-09-07 07:15:28 +0300

    service properties

--- kw-service/src/main/java/fi/william/kwservice/electricity/ElectricityController.java
+++ kw-service/src/main/java/fi/william/kwservice/electricity/ElectricityController.java
@@ -4,8 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,9 +20,9 @@ public class ElectricityController {
         this.electricityService = electricityService;
     }
 
-    @GetMapping("/day/{day}")
+    @GetMapping
     public ResponseEntity<List<ElectricityDto>> getAllOnDay(
-        @PathVariable String day
+        @RequestParam String day
     ) {
         log.info("Received request to get all electricity data on day: {}", day);
         List<ElectricityDto> data = electricityService.getAllOnDay(day);
