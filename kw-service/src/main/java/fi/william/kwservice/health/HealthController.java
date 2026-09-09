3407b1d0c42ce43e64acc117564c0c74a5f86ad8
William Lempinen <william.lempinen@twoday.com>
2026-09-09 17:31:21 +0300

    kw-service: health, pom

--- kw-service/src/main/java/fi/william/kwservice/health/HealthController.java
+++ kw-service/src/main/java/fi/william/kwservice/health/HealthController.java
@@ -1,4 +1,13 @@
 package fi.william.kwservice.health;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<String> getHealth() {
+        return ResponseEntity.ok("Healthy");
+    }
 }
